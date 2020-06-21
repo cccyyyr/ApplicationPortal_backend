6a094b71ebea07f4a74608cd11e27f7c808ffad6
Cathy Chen <cathychen@Cathys-MacBook-Pro.local>
2020-06-21 19:41:30 +0800

    Deletion

--- src/test/java/com/cathychen/ApplicationPortal/tests/baseTest/SpringTestCase.java
+++ src/test/java/com/cathychen/ApplicationPortal/tests/baseTest/SpringTestCase.java
@@ -1,14 +1,10 @@
 package com.cathychen.ApplicationPortal.tests.baseTest;
 
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-
 public abstract class SpringTestCase {
-	Logger logger = LoggerFactory.getLogger(this.getClass());
 } 
